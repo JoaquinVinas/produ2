5d1594d17e61ca3a5716684e62163bafeedef87b
JoaquinVinas <134808958+JoaquinVinas@users.noreply.github.com>
2024-11-17 23:33:43 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+
+        // Ajustar la posición del scroll
+        window.scrollTo({
+            top: target.offsetTop - 20, // Espacio extra
+            behavior: 'smooth',
+        });
+    });
+});
